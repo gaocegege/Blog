a3b128cb832d33f134a8648810d0376f259fc1c2
Ce Gao <gaocegege@hotmail.com>
2021-03-02 21:57:35 +0800

    feat: Add a new post
    Signed-off-by: Ce Gao <gaocegege@hotmail.com>

--- _posts/helper.py
+++ _posts/helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import time
 import os.path
@@ -7,8 +7,8 @@ def writeTemplate(objname):
 	today = time.strftime('%Y-%m-%d-')
 	filename = today + objname + '.md'
 	if os.path.exists(filename):
-		print "Error: file content."
-		return;
+		print("Error: file content.")
+		return
 	f = open(filename, 'w')
 	f.write("---\nlayout: post\ntitle: \"\"\ndescription: \nheadline:\nmodified: " + today[:-1] + "\ncategory: \ntags: []\nimagefeature:\nmathjax: false\nchart:\ncomments: true\nfeatured: true\n---\n")
 	f.write("""## License
@@ -16,12 +16,12 @@ def writeTemplate(objname):
 - This article is licensed under [CC BY-NC-SA 3.0](https://creativecommons.org/licenses/by-nc-sa/3.0/).
 - Please contact me for commercial use.
 """)
-	print "Write Done."
+	print("Write Done.")
 
 def main():
 	if len(sys.argv) != 2:
-		print "Error: argv."
-		return;
+		print("Error: argv.")
+		return
 	filename = sys.argv[1]
 	writeTemplate(filename)
 
